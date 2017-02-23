b73e364b0c5240d464168bcc568dd1b0daae7798
genesis <genesis@info@local.com>
2017-02-23 14:01:50 -0400

    nuevo

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include './plantilla/header.php';
+?>
+
+     <div class="parallax-container">
+         <div class="parallax"><img src="css/images/sistemas-3.jpg"></div>
+  </div>
+  <div class="section white">
+    <div class="row container">
+      <h2 class="header">Parallax</h2>
+      <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+    </div>
+  </div>
+  <div class="parallax-container">
+      <div class="parallax"><img src="css/images/computer-engineering-science-tech-matrix-images-221667.jpg"></div>
+  </div>
+
+</div>
+<?php include './plantilla/footer.php'; ?>
---
+++ plantilla/header.php
@@ -0,0 +1,31 @@
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Devweb</title>
+        <link rel="stylesheet" href="estilos.css">
+        <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="" alt=""></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="blog.php">Blog</a></li>
+					<li><a href="acerca_de.php">Acerca de</a></li>
+					<li><a href="contacto.php">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+  <div class="site_wrapper">
+            <!-- end header!-- end header -->
